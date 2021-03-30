0cbb393e1abc9ec1e2d55044a38d40050275c2ab
Serggin <serggin1@yandex.ru>
2021-03-30 15:51:24 +0300

    tickets initial commit

---
+++ src/client/tickets/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//import '../../assets/css/style.css';
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>Экскурсия. Билеты</h1>
+    </div>
+  );
+}
