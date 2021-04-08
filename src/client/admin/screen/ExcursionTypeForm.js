d06e3514786424d05d52acdac7ff807f7ff4828b
Serggin <serggin1@yandex.ru>
2021-04-08 22:29:10 +0300

    EscursionTypeForm draft 2

--- src/client/admin/screen/ExcursionTypeForm.js
+++ src/client/admin/screen/ExcursionTypeForm.js
@@ -1,21 +1,72 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import FormTextElement from '../../components/form/FormTextElement';
 import FormTextAreaElement from '../../components/form/FormTextAreaElement';
 
 export default function EscursionTypeForm({typeData}) {
   const [name, setName] = useState(typeData.name);
   const [description, setDescription] = useState(typeData.description);
+  const [participants, setParticipants] = useState(typeData.participants);
+
+  const options = JSON.parse(typeData.options);
+  const {tickets, schedule} = options;
+  const [fullcost, setFullcost] = useState(options.tickets.fullcost);
+  const [discount, setDiscount] = useState(options.tickets.discount);
+
+  const [timeStart, setTimeStart] = useState('');
+  const [timeEnd, setTimeEnd] = useState('');
+  const [intervalMinutes, setIntervalMinutes] = useState(0);
+  const [error, setError] = useState('');
+
   const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+
+  }, []);
+
+  useEffect(() => {
+    const oldReady = ready;
+    const newReady = name.trim().length>0 && timeStart.trim().length>0 && timeEnd.trim().length>0
+      && participants>0 && fullcost>0 && discount>0 && intervalMinutes;
+    if (oldReady !== newReady) {
+      setReady(newReady);
+    }
+  }, [name, participants, fullcost, discount, timeStart, timeEnd, intervalMinutes]);
+
+  const validate = () => {
+    return true;
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (validate()) {
+
+    }
   }
 
   return (
     <div className="excursion-type-form">
       <form onSubmit={handleSubmit}>
         <FormTextElement label="Наименование" required={true} onChange={setName} defaultValue={typeData.name}/>
-        <FormTextAreaElement label="Описание" onChange={setName} defaultValue={typeData.description}/>
+        <FormTextAreaElement label="Описание" onChange={setDescription} defaultValue={typeData.description}/>
+        <FormTextElement label="Участников" required={true} onChange={setParticipants} defaultValue={typeData.participants}/>
+        <div className="form-element">Цена билета (руб.)</div>
+        <FormTextElement label="полный" required={true} onChange={setFullcost} defaultValue={options.tickets.fullcost}/>
+        <FormTextElement label="льготный" required={true} onChange={setDiscount} defaultValue={options.tickets.discount}/>
+        <hr />
+        <div className="form-element">
+          <label>Тип расписания{<span className="required" />}: </label>
+          <span>стандартное</span>
+        </div>
+        {typeData.schedule_type === 'grid' &&
+          <>
+          <FormTextElement label="время начала" required={true} defaultValue={timeStart}
+                           placeholder="чч:мм" onChange={setTimeStart} />
+          <FormTextElement label="время окончания" required={true} defaultValue={timeEnd}
+                           placeholder="чч:мм" onChange={setTimeEnd} />
+          <FormTextElement label="интервал" required={true} defaultValue={intervalMinutes}
+                           onChange={setIntervalMinutes} />
+          </>
+        }
         <input type="submit" value="Сохранить" disabled={!ready}/>
       </form>
     </div>
--- src/client/components/form/FormTextElement.js
+++ src/client/components/form/FormTextElement.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export default function FormTextElement({label, required=false, onChange, defaultValue}) {
+export default function FormTextElement({label, required=false, onChange, defaultValue, value, placeholder}) {
   return (
     <div className="form-element">
       <label>{label}{required && <span className="required" />}: </label>
-      <input type="text" onChange={event => onChange(event.target.value)} defaultValue={defaultValue} />
+      <input type="text" defaultValue={defaultValue} value={value}
+             placeholder={placeholder}
+             onChange={event => onChange(event.target.value)}/>
     </div>
   );
 }
